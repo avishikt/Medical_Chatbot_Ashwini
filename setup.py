23513d240f59d1a2704e5533727d2bd5dc1d9113
Avishikta Das <dasavishikta815@gmail.com>
2025-03-08 17:39:57 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Chatbot_Ashwini',
+    version= '0.0.0',
+    author= 'Avishikta Das',
+    author_email= 'dasavishikta3@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
